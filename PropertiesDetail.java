d45e6581cb781dbb9f95ef348ef776c5fdd33ac7
wananlaxiaoyu <kaifazhanghao@gmail.com>
2023-07-13 20:21:24 +0900

    no message

---
+++ PropertiesDetail.java
@@ -0,0 +1,20 @@
+public class PropertiesDetail {
+    public static void main(String[] args) {
+
+
+peron p1 = new peron();
+        System.out.println("当前这个人的信息");
+        System.out.println("age=" + p1.age + "name=" + p1.name + "sal=" + p1.sal + "isPass=" + p1.isPass );
+    }
+
+
+
+        }
+class peron{
+    int age;
+    String name;
+    double sal;
+    boolean isPass;
+
+
+}
